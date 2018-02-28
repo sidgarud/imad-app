e3be9c7ecc0da02753213ed2628fdad5a9b4a9e3
sidgarud <33521515+sidgarud@users.noreply.github.com>
2018-02-28 13:31:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,16 +61,15 @@ var htmlTemplate=`
 return htmlTemplate;
 }
 
-app.get('/articleName', function (req, res){
-    res.send(createTemplate(articles[articleName]));
-});
-
 app.get('/', function (req, res) {
   var articleName=req.params.articleName;
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
- 
+app.get('/articleName', function (req, res){
+    res.send(createTemplate(articles[articleName]));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

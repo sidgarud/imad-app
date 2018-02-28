06bd00d5b08ff418ef3b4a34ed19893e2e682947
sidgarud <33521515+sidgarud@users.noreply.github.com>
2018-02-28 11:11:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-1', function (req, res) {
+  res.send('article 1 served');
+});
+
+app.get('/article-2', function (req, res) {
+  res.send('article 2 served');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

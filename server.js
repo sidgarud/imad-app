f6de7e74bc831559e0351846268173a648ee0c63
sidgarud <33521515+sidgarud@users.noreply.github.com>
2018-02-28 11:25:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-2', function (req, res) {

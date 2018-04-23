dd370699a3485f3473c4e4cb33c2e7e16fa1356c
sidgarud <33521515+sidgarud@users.noreply.github.com>
2018-04-23 23:02:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -232,8 +232,8 @@ app.get('/ui/:fileName', function (req, res) {
 });
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+var port = 80; // Use 8080 for local development because you might already have apache running on 80
+app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
 

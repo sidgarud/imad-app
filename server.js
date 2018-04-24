9cc00cd29b8d559eb5e09646f60587fc4ee4a495
sidgarud <33521515+sidgarud@users.noreply.github.com>
2018-04-24 20:53:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,6 +83,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter = counter + 1;
+  res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res) {
   // articleName == article-one
   // articles[articleName] == {} content object for article one
@@ -92,6 +98,10 @@ app.get('/:articleName', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {

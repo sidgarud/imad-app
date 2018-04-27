56cca4a566c88029528775aae3a5423971d85ed0
sidgarud <33521515+sidgarud@users.noreply.github.com>
2018-04-27 10:32:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,25 @@
-console.log('Loaded!');
-
-// Change the text of the main-text div
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New value';
-
-// Move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-};
-
 // Counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function () {
-  // Make a request to the counter endpoint
-  // Capture the response if the request was succesfully completed
-  // Render the response in the HTML
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();          
+          }
+      }  
+      // Not done yet
+    };
+    
+    // Make the request
+    request.open('GET', 'http://coco98.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
